14f5718f08198b388d0b33be0191c3900cb938ce
Shreyas Bhandary <168298221+shrey-bhandary@users.noreply.github.com>
2025-08-25 00:03:55 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
-import { MapPin, Calendar, Star } from 'lucide-react';
+import { MapPin, Star } from 'lucide-react';
 
 const Home: React.FC = () => {
   const destinations = [
